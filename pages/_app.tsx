b64c06812ced5a6e1cbe5532151b03e0a7d1d519
lsprr <16653744+lsprr@users.noreply.github.com>
2023-04-03 00:11:37 -0400

    Update website content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,9 +16,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Luis Parra</title>
         <meta property="og:title" content="Luis Parra" />
-        <meta name="description" content="Luis Parra, a dedicated Software Engineer, takes pleasure in resolving challenges and streamlining digital products to ensure a user-friendly experience." key="desc" />
+        <meta name="description" content="Meet Luis, a passionate Software Engineer specializing in web development since 2006. Inspired by RuneScape, he now creates inclusive, accessible digital experiences for all. Join his journey for a dose of inspiration and innovation!" key="desc" />
         <meta property="og:description"
-          content="Luis Parra, a dedicated Software Engineer, takes pleasure in resolving challenges and streamlining digital products to ensure a user-friendly experience." />
+          content="Meet Luis, a passionate Software Engineer specializing in web development since 2006. Inspired by RuneScape, he now creates inclusive, accessible digital experiences for all. Join his journey for a dose of inspiration and innovation!" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
