de2c50830af7e9f7ac2dc17eb80ec6850c48bb84
lsprr <16653744+lsprr@users.noreply.github.com>
2023-04-07 13:54:29 -0400

    Fixing Dark Mode

--- hooks/useDarkMode.tsx
+++ hooks/useDarkMode.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 type Theme = 'light' | 'dark';
 
 const useDarkMode = (): [Theme, () => void] => {
-    const [theme, setTheme] = useState<Theme>('dark');
+    const [theme, setTheme] = useState<Theme>('light');
 
     useEffect(() => {
         const root = window.document.documentElement;
