5f49966c29d4dbebe286a5b3dbb3d88e3a83a484
lsprr <16653744+lsprr@users.noreply.github.com>
2023-08-14 01:43:53 -0400

    Fixed Background Attachment Hack

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
         <Html lang="en">
             <Head />
             <body>
+                <div className='bg' />
                 <Main />
                 <NextScript />
             </body>
