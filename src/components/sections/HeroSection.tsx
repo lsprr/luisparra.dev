dabdaf4907e9457b16711a468951ed72da9a1daa
lsprr <16653744+lsprr@users.noreply.github.com>
2023-10-11 15:47:14 -0400

    Minor corrections

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -2,7 +2,7 @@ export const HeroSection = () => {
     return (
         <section>
             <header>
-                <h1 className="text-bold">Join Sir Luis Parra in Camelot's Court!</h1>
+                <h1 className="text-bold">Join Sir Luis Parra in Camelot&apos;s Court!</h1>
                 <p className="text-italic">Crafting Web Realms, One Quest at a Time</p>
             </header>
         </section>
