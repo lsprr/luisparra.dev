d645da0a749b9050f687264a88f3184ee7ecb090
lsprr <16653744+lsprr@users.noreply.github.com>
2023-10-11 15:51:43 -0400

    Fix label-content-name-mismatch issue

--- src/components/ProfessionalLinks.tsx
+++ src/components/ProfessionalLinks.tsx
@@ -10,7 +10,7 @@ export const ProfessionalLinks = () => {
                             <circle cx="4" cy="4" r="2"></circle>
                         </svg>
                     </div>
-                    <a target="_blank" rel="nofollow noreferrer noopener" className="external text" aria-label="Link to my LinkedIn profile" href="https://linkedin.com/in/lsprr">
+                    <a target="_blank" rel="nofollow noreferrer noopener" className="external text" aria-label="LinkedIn" href="https://linkedin.com/in/lsprr">
                         <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="partner-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
                         <h2 className="partner-name">LinkedIn</h2>
                         <p className="partner-tagline">Let&apos;s Network on LinkedIn!</p>
@@ -27,7 +27,7 @@ export const ProfessionalLinks = () => {
                             <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                         </svg>
                     </div>
-                    <a target="_blank" rel="nofollow noreferrer noopener" className="external text" aria-label="Link to my GitHub profile" href="https://github.com/lsprr">
+                    <a target="_blank" rel="nofollow noreferrer noopener" className="external text" aria-label="GitHub" href="https://github.com/lsprr">
                         <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="partner-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
                         <h2 className="partner-name">GitHub</h2>
                         <p className="partner-tagline">Browse My GitHub Repos!</p>
