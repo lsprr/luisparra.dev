293617a18a242b8d8ace4ead18bb2a76e5ad09b7
lsprr <16653744+lsprr@users.noreply.github.com>
2023-10-11 17:18:16 -0400

    Minor corrections

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en" data-theme="light">
-            <Head />
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;300;400&family=PT+Serif:wght@400;700&display=swap" rel="stylesheet" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
