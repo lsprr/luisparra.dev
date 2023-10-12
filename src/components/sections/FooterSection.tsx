55de902faca27eb16607c2dfce411ac49fb57ae5
lsprr <16653744+lsprr@users.noreply.github.com>
2023-10-12 15:08:54 -0400

    Update FooterSection.tsx and README.md

--- src/components/sections/FooterSection.tsx
+++ src/components/sections/FooterSection.tsx
@@ -2,8 +2,8 @@ export const FooterSection = () => {
     const year: number = new Date().getFullYear();
 
     return (
-        <footer className="simple flex flex-col" aria-label="Site Footer">
-            <p className="text-bold mb-0">&copy; {year}. Luis Parra. All rights reserved.</p>
+        <footer aria-label="Site Footer">
+            <p className="text-bold">&copy; {year}. Luis Parra. All rights reserved.</p>
         </footer>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,9 +26,9 @@ export default function Home() {
                     <AfterHours />
                     <TravelDiaries />
                     <FAQSection />
-                    <FooterSection />
                 </aside>
             </div>
+            <FooterSection />
         </>
     );
 }
