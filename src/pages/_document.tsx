887eaa76273fe454757e093791936301978c04c5
lsprr <16653744+lsprr@users.noreply.github.com>
2023-10-18 23:18:14 -0400

    Minor corrections

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en" data-theme="light">
-            <Head>
-                <link rel="stylesheet" href="https://use.typekit.net/kfx0vct.css" />
-            </Head>
+            <Head />
             <body>
                 <Main />
                 <NextScript />
