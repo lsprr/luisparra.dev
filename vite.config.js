fcc2ff64536ff96b6a2e9b9d2284f290252eeefc
lsprr <16653744+lsprr@users.noreply.github.com>
2024-02-23 17:57:07 -0500

    Serve images that are appropriately-sized to save cellular data and improve load time

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite';
+import path from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-export default defineConfig(() => {
-    return {
-        plugins: [
-            ViteImageOptimizer({
-                webp: {
-                    // https://sharp.pixelplumbing.com/api-output#webp
-                    lossless: true,
-                },
-            }),
-        ],
-    };
+export default defineConfig({
+    resolve: {
+        '@': path.resolve(__dirname, './src'),
+        '@assets': path.resolve(__dirname, './public/assets/'),
+    },
+    plugins: [
+        ViteImageOptimizer({
+            webp: {
+                // https://sharp.pixelplumbing.com/api-output#webp
+                lossless: true,
+            },
+        }),
+    ],
 });
