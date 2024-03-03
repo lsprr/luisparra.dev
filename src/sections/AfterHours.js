853299adccff1dcf0c19d2d5c2a81ce8c4b489eb
lsprr <16653744+lsprr@users.noreply.github.com>
2024-03-03 17:55:17 -0500

    Refactored HTML structure for better accessibility and readability

--- src/sections/AfterHours.js
+++ src/sections/AfterHours.js
@@ -8,58 +8,56 @@ const AfterHours = () => {
             every effort to travel, both domestically and overseas. 
         </p>
         <h3>My Favorite Places in the World (so far)</h3>
-        <table>
-            <tbody>
-                <tr>
-                    <td>
-                        <a href="https://en.wikipedia.org/wiki/Mahahual">Mahahual, Costa Maya</a>
-                    </td>
-                    <td>
-                        A place where sunlight abounds and the climate is ideal for both seaside appeal and "chef's kiss" weather. 
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <a href="https://en.wikipedia.org/wiki/Willemstad">Willemstad, Curaçao</a>
-                    </td>
-                    <td>
-                        This vivid Caribbean treasure offers a kaleidoscope of hues, combining island charm with Dutch influence to create an amazing experience.
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <a href="https://en.wikipedia.org/wiki/Punta_Cana">Punta Cana, Dominican Republic</a>
-                    </td>
-                    <td>
-                        Relax in paradise as pristine beaches meet turquoise waters, forming a refuge where relaxation and adventure coexist seamlessly.
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <a href="https://en.wikipedia.org/wiki/Lisbon">Lisboa, Portugal</a>
-                    </td>
-                    <td>
-                        A colorful tapestry of Portuguese culture leads from narrow cobblestone alleyways, combining modern charm with historical charm.                     
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <a href="https://en.wikipedia.org/wiki/Porto">Porto, Portugal</a>
-                    </td>
-                    <td>
-                        A charming fusion of energetic modernity and old world charm, the Douro River meanders through old neighborhoods here, beckoning me to experience the true spirit of Portuguese culture.
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <a href="https://en.wikipedia.org/wiki/Funchal">Funchal, Madeira</a>
-                    </td>
-                    <td>
-                        Embrace the charm of cobbled streets, vibrant markets, and breathtaking ocean views in this botanical haven nestled between lush hills and the sparkling Atlantic. 
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="custom-table">
+            <div class="table-row">
+                <div class="table-cell">
+                    <a href="https://en.wikipedia.org/wiki/Mahahual">Mahahual, Costa Maya</a>
+                </div>
+                <div class="table-cell">
+                    A place where sunlight abounds and the climate is ideal for both seaside appeal and "chef's kiss" weather. 
+                </div>
+            </div>
+            <div class="table-row">
+                <div class="table-cell">
+                    <a href="https://en.wikipedia.org/wiki/Willemstad">Willemstad, Curaçao</a>
+                </div>
+                <div class="table-cell">
+                    This vivid Caribbean treasure offers a kaleidoscope of hues, combining island charm with Dutch influence to create an amazing experience. 
+                </div>
+            </div>
+            <div class="table-row">
+                <div class="table-cell">
+                    <a href="https://en.wikipedia.org/wiki/Punta_Cana">Punta Cana, Dominican Republic</a>
+                </div>
+                <div class="table-cell">
+                    Relax in paradise as pristine beaches meet turquoise waters, forming a refuge where relaxation and adventure coexist seamlessly. 
+                </div>
+            </div>
+            <div class="table-row">
+                <div class="table-cell">
+                    <a href="https://en.wikipedia.org/wiki/Lisbon">Lisboa, Portugal</a>
+                </div>
+                <div class="table-cell">
+                    A colorful tapestry of Portuguese culture leads from narrow cobblestone alleyways, combining modern charm with historical charm. 
+                </div>
+            </div>
+            <div class="table-row">
+                <div class="table-cell">
+                    <a href="https://en.wikipedia.org/wiki/Porto">Porto, Portugal</a>
+                </div>
+                <div class="table-cell">
+                    A charming fusion of energetic modernity and old world charm, the Douro River meanders through old neighborhoods here, beckoning me to experience the true spirit of Portuguese culture. 
+                </div>
+            </div>
+            <div class="table-row">
+                <div class="table-cell table-cell-b-bottom">
+                    <a href="https://en.wikipedia.org/wiki/Funchal">Funchal, Madeira</a>
+                </div>
+                <div class="table-cell table-cell-b-bottom">
+                    Embrace the charm of cobbled streets, vibrant markets, and breathtaking ocean views in this botanical haven nestled between lush hills and the sparkling Atlantic. 
+                </div>
+            </div>
+        </div>
     </section> 
     `
 }
--- src/sections/Technologies.js
+++ src/sections/Technologies.js
@@ -3,41 +3,40 @@ const Technologies = () => {
     <section>
         <h2>My Favorite Technologies</h2>
         <p>A non-exhaustive list of technologies that I enjoy using.</p>
-        <table>
-        <tbody>
-            <tr>
-                <td><a href="https://www.typescriptlang.org/">TypeScript</a></td>
-                <td>
-                    I have used TypeScript for years and enjoy its static typing and versatility in creating 
-                    scalable web applications. It remains my go-to for a robust development experience. 
-                </td>
-            </tr>
-            <tr>
-                <td><a href="https://www.deque.com/axe/">Axe</a></td>
-                <td>
-                    For years, I have used axe (accessibility testing tools) in my workflow, leveraging its 
-                    tremendous capabilities to ensure web apps are accessible. It remains my preferred method 
-                    for improving accessibility and providing inclusive user experiences.                 
-                </td>
-            </tr>
-            <tr>
-                <td><a href="https://vitejs.dev/">Vite</a></td>
-                <td>
-                    A fantastic JS bundler that can handle web applications of just
-                    about any scale.
-                </td>
-            </tr>
-            <tr>
-                <td>
+        <div class="custom-table">
+            <div class="table-row">
+                <div class="table-cell">
+                    <a href="https://www.typescriptlang.org/">TypeScript</a>
+                </div>
+                <div class="table-cell">
+                    I have used TypeScript for years and enjoy its static typing and versatility in creating scalable web applications. It remains my go-to for a robust development experience.
+                </div>
+            </div>
+            <div class="table-row">
+                <div class="table-cell">
+                    <a href="https://www.deque.com/axe/">Axe</a>
+                </div>
+                <div class="table-cell">
+                    For years, I have used axe (accessibility testing tools) in my workflow, leveraging its tremendous capabilities to ensure web apps are accessible. It remains my preferred method for improving accessibility and providing inclusive user experiences.
+                </div>
+            </div>
+            <div class="table-row">
+                <div class="table-cell">
+                    <a href="https://vitejs.dev/">Vite</a>
+                </div>
+                <div class="table-cell">
+                    A fantastic JS bundler that can handle web applications of just about any scale.
+                </div>
+            </div>
+            <div class="table-row">
+                <div class="table-cell table-cell-b-bottom">
                     <a href="https://prettier.io/">Prettier</a>
-                </td>
-                <td>
-                    Lets me focus on code and not the formatting. Very simple tool that
-                    makes me much more productive.
-                </td>
-            </tr>
-        </tbody>
-        </table>
+                </div>
+                <div class="table-cell table-cell-b-bottom">
+                    Lets me focus on code and not the formatting. Very simple tool that makes me much more productive.
+                </div>
+            </div>
+        </div>
     </section> 
     `
 }
