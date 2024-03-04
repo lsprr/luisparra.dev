74b596f26084a4a507e5a4655876020f3f352076
lsprr <16653744+lsprr@users.noreply.github.com>
2024-03-03 21:03:42 -0500

    Minor corrections

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,14 +2,12 @@ const Footer = () => {
     const year = new Date().getFullYear();
 
     return `
-    <footer>
-        <nav>
-            <a href="https://www.linkedin.com/in/lsprr" target="_blank" rel=”noopener noreferrer">Linkedin</a>
-            <a href="https://github.com/lsprr" target="_blank" rel=”noopener noreferrer">Github</a>
-            <a href="https://github.com/lsprr/luisparra.dev" target="_blank" rel=”noopener noreferrer">Website Source Code</a>
-        </nav>
-        <p>Copyright ${year} Luis Parra</p>
-    </footer>
+    <nav>
+        <a href="https://www.linkedin.com/in/lsprr" target="_blank" rel=”noopener noreferrer">Linkedin</a>
+        <a href="https://github.com/lsprr" target="_blank" rel=”noopener noreferrer">Github</a>
+        <a href="https://github.com/lsprr/luisparra.dev" target="_blank" rel=”noopener noreferrer">Website Source Code</a>
+    </nav>
+    <p>Copyright ${year} Luis Parra</p>
     `
 }
 
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,22 +2,20 @@ import { getImageUrl } from "../helpers";
 
 const Header = () => {
     return `
-    <header>
-        <div class="logo-container">
-            <img
-                src=${getImageUrl('bear.png')}
-                width="250"
-                height="250"      
-                alt="A white bear wearing a hat and shorts, standing on a field."      
-            />
-        </div>
-        <h1>Luis Parra</h1>
-        <p>Personal Website of Luis Parra</p>
-        <nav>
-            <a href="https://www.linkedin.com/in/lsprr" target="_blank" rel=”noopener noreferrer">Linkedin</a>
-            <a href="https://github.com/lsprr" target="_blank" rel=”noopener noreferrer">Github</a>
-        </nav>
-    </header> 
+    <div class="logo-container">
+        <img
+            src=${getImageUrl('bear.png')}
+            width="250"
+            height="250"      
+            alt="A white bear wearing a hat and shorts, standing on a field."      
+        />
+    </div>
+    <h1>Luis Parra</h1>
+    <p>Personal Website of Luis Parra</p>
+    <nav>
+        <a href="https://www.linkedin.com/in/lsprr" target="_blank" rel=”noopener noreferrer">Linkedin</a>
+        <a href="https://github.com/lsprr" target="_blank" rel=”noopener noreferrer">Github</a>
+    </nav>
     `
 }
 
